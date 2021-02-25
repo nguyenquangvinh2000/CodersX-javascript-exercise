1a93f55570d6ab7297bd4fe5714b712030d4c744
nguyenquangvinh2000 <vinhng74@gmail.com>
2021-02-25 18:42:08 +0700

    Update app.js

--- manager_app_CLI/app.js
+++ manager_app_CLI/app.js
@@ -7,34 +7,29 @@ function loadData(){
   contacts = JSON.parse(fileContent);
 }
 function showMenu(){
-  console.log('1. show all contact');
-  console.log('2. add new contact');
+  console.log('1. add new contact');
+  console.log('2. show all contact');
   console.log('3. save and exit');
   var choose = readlineSync.question('> ');
   switch (choose) {
     case '1':
-      showAllContact();
+      createNewContact();
       showMenu();
       break;
     case '2':
-      createNewContact();
+      showAllContact();
       showMenu();
       break;
     case '3':
       saveAndExit();
       break;
     default:
-      console.log('wrong option');
+      console.log('wrong choose');
       showMenu();
       break;
 
   }
 
-}
-function showAllContact(){
-  for(var contact of contacts){
-    console.log(conacts.name, contacts.age);
-  }
 }
 function  createNewContact(){
   var name = readlineSync.question('name: ');
@@ -48,13 +43,17 @@ function  createNewContact(){
 
 
 }
+function showAllContact(){
+  for(var contact of contacts){
+    console.log(contact.name, contact.age);
+  }
+}
 function saveAndExit(){
   var content = JSON.stringify(contacts);
   fs.writeFileSync('./data.json',content, {encoding: 'utf8'});
 }
 function main(){
   loadData();
-  console.log(contacts);
   showMenu();
 }
 main();
